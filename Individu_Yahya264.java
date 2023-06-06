74960c2d214dbf61927e31d56da9ca17b53d3f4a
YahyaKhaliman <134560596+YahyaKhaliman@users.noreply.github.com>
2023-06-06 12:46:52 +0700

    Add files via upload

---
+++ Individu_Yahya264.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author yahya
+ */
+public class Individu_Yahya264 {
+       public static void main(String[] args) {
+        String nama = "Yahya Khaliman Indrayana";
+        String nim = "2113010264";
+           
+        System.out.println("==== Hasil ====");
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+    }
+}
