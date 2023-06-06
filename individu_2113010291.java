bfa21cc3d334b5cbe372c7ae71fdeb3bb6606198
Kresna11173 <92725895+Kresna11173@users.noreply.github.com>
2023-06-06 10:37:54 +0700

    Create individu_2113010291.java

---
+++ individu_2113010291.java
@@ -0,0 +1,18 @@
+public class individu_2113010291 {
+    public static void main(String[] args) throws Exception {
+        
+        String nama = "ilham kresna mukti";
+        int nim = 2113010291;
+
+
+        // new commit
+        String pesan = String.format("\nnama saya adalah = %s, dan nim ku = %d", nama, nim);
+        
+
+        System.out.println(pesan);
+        
+
+        
+    
+    }
+}
