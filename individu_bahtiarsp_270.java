261abd6f57a1f2247d611f42bf05771546edad96
bahtiarsp <133626818+bahtiarsp@users.noreply.github.com>
2023-06-06 14:53:54 +0700

    Add files via upload

---
+++ individu_bahtiarsp_270.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String nama = "Bahtiar Surya Pinilih";
+        String nim = "2113010270";
+
+        System.out.println(nama);
+        System.out.println(nim);
+    }
+}
