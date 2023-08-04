590f3e375a6f5ec7cad34b05ffc58cdf26bc1bf7
bahtiarsp <133626818+bahtiarsp@users.noreply.github.com>
2023-08-04 23:36:30 +0700

    Add files via upload
    GUI TRY1

---
+++ GUI_2/src/main/java/com/mycompany/gui_2/GUI_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gui_2;
+
+/**
+ *
+ * @author Bachtiar
+ */
+public class GUI_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
