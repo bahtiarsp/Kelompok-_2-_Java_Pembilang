0d03dd564d092d387e73fff0e630349f5b11a949
bahtiarsp <133626818+bahtiarsp@users.noreply.github.com>
2023-08-08 15:00:45 +0700

    Update individu_bahtiarsp_270.java

--- individu_bahtiarsp_270.java
+++ individu_bahtiarsp_270.java
@@ -1,4 +1,4 @@
-public class Main {
+public class individu_bahtiarsp_270 {
     public static void main(String[] args) {
         String nama = "Bahtiar Surya Pinilih";
         String nim = "2113010270";
